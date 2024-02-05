abeab76299cb0433c7464ac0cac545249c06e00a
MLDODSON <paola@dominicansoul.com>
2024-02-05 09:42:33 -0600

    Branch 6
    Branch 6 - Setting up and exporting the actions.js file.

--- cart/src/context.jsx
+++ cart/src/context.jsx
@@ -1,6 +1,15 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 import reducer from "./reducer";
 
+import { 
+    CLEAR_CART, 
+    REMOVE, 
+    INCREASE, 
+    DECREASE, 
+    LOADING, 
+    DISPLAY_ITEMS 
+} from "./actions";
+
 const AppContext = createContext();
 
 const initialState = {
